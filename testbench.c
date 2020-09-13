dd7785eb221b8494a12784261180ddb20efcda1f
TejashwiniMG <tganachari@gmail.com>
2020-09-13 23:01:28 +0530

    add

--- testbench.c
+++ testbench.c
@@ -2,54 +2,57 @@
 #include <stdlib.h>
 #include "wprpgm.h"
 #include "wprpgm.c"
-#define EXPECT_EQ(a, b) {\
+#define EXPECT_EQ(a, b, c) {\
     if (a==b)\
-        printf("PASSED\n");\
+        fprintf(c,"PASSED\n");\
     else \
-       printf("FAILED\n");\
+       fprintf(c,"FAILED\n");\
 }
+
+FILE *fptr;
+
 void TC1_1(void)
 {
-   printf("Test case 1\t");
+   fprintf(fptr,"Test case 1\t");
    int ret=wiper(1,50);
    int exp=1;
-   EXPECT_EQ(exp,ret);
+   EXPECT_EQ(exp,ret,fptr);
 
 }
 
 void TC1_2(void)
 {
-   printf("Test case 2\t");
+   fprintf(fptr,"Test case 2\t");
    int ret=wiper(0,50);
    int exp=0;
-   EXPECT_EQ(exp,ret);
+    EXPECT_EQ(exp,ret,fptr);
 
 }
 
 void TC1_3(void)
 {
-   printf("Test case 3\t");
+   fprintf(fptr,"Test case 3\t");
    int ret=wiper(1,600);
    int exp=75;
-   EXPECT_EQ(exp,ret);
+    EXPECT_EQ(exp,ret,fptr);
 
 }
 
 void TC1_4(void)
 {
-   printf("Test case 4\t");
+   fprintf(fptr,"Test case 4\t");
    int ret=wiper(1,300);
    int exp=50;
-   EXPECT_EQ(exp,ret);
+    EXPECT_EQ(exp,ret,fptr);
 
 }
 
 void TC1_5(void)
 {
-   printf("Test case 5\t");
+   fprintf(fptr,"Test case 5\t");
    int ret=wiper(1,150);
    int exp=25;
-   EXPECT_EQ(exp,ret);
+    EXPECT_EQ(exp,ret,fptr);
 
 }
 
@@ -57,11 +60,13 @@ int main(void)
 {
 //UnityBegin();
 //RUN_TEST(TC1_1);
+fptr=fopen("log.txt","w");
 TC1_1();
 TC1_2();
 TC1_3();
 TC1_4();
 TC1_5();
+fclose(fptr);
 //printf("pass");
 return 0;
 //return UnityEnd();
